a2c61e693b1fae2569cf7fd514f4497cee88aa78
nikit <nikit.periwal@gmail.com>
2021-06-20 17:49:27 +0530

    Updated AI Model

--- twitter_sentiment/classifier.py
+++ twitter_sentiment/classifier.py
@@ -2,12 +2,9 @@ import re
 import pickle
 import numpy as np
 import pandas as pd
-from tensorflow.keras.models import load_model
+from tensorflow.keras import Sequential
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
-with open('twitter_sentiment/models/Tokenizer.pickle', 'rb') as file:
-    tokenizer = pickle.load(file)
-model = load_model('twitter_sentiment/models/Twitter-Sentiment-BiLSTM.h5')
+from tensorflow.keras.layers import Bidirectional, GlobalMaxPool1D, Dense, LSTM, Conv1D, Embedding
 
 # Reading contractions.csv and storing it as a dict.
 contractions = pd.read_csv('twitter_sentiment/models/contractions.csv', index_col='Contraction')
@@ -24,10 +21,10 @@ sequencePattern = r"(.)\1\1+"
 seqReplacePattern = r"\1\1"
 
 # Defining regex for emojis
-smileemoji = r"[8:=;]['`\-]?[)d]+"
-sademoji = r"[8:=;]['`\-]?\(+"
-neutralemoji = r"[8:=;]['`\-]?[\/|l*]"
-lolemoji = r"[8:=;]['`\-]?p+"
+emoji_smile = r"[8:=;]['`\-]?[)d]+"
+emoji_sad = r"[8:=;]['`\-]?\(+"
+emoji_neutral = r"[8:=;]['`\-]?[\/|l*]"
+emoji_lol = r"[8:=;]['`\-]?p+"
 
 
 def preprocess_apply(tweet):
@@ -42,10 +39,10 @@ def preprocess_apply(tweet):
     tweet = re.sub(sequencePattern, seqReplacePattern, tweet)
     # Replace all emojis.
     tweet = re.sub(r'<3', '<heart>', tweet)
-    tweet = re.sub(smileemoji, '<smile>', tweet)
-    tweet = re.sub(sademoji, '<sadface>', tweet)
-    tweet = re.sub(neutralemoji, '<neutralface>', tweet)
-    tweet = re.sub(lolemoji, '<lolface>', tweet)
+    tweet = re.sub(emoji_smile, '<smile>', tweet)
+    tweet = re.sub(emoji_sad, '<sadface>', tweet)
+    tweet = re.sub(emoji_neutral, '<neutralface>', tweet)
+    tweet = re.sub(emoji_lol, '<lolface>', tweet)
     for contraction, replacement in contractions_dict.items():
         tweet = tweet.replace(contraction, replacement)
     # Remove non-alphanumeric and symbols
@@ -55,11 +52,35 @@ def preprocess_apply(tweet):
     return tweet
 
 
+def load_model():
+    embedding_layer = Embedding(input_dim=272836,
+                                output_dim=100,
+                                input_length=60)
+
+    model = Sequential([
+        embedding_layer,
+        Bidirectional(LSTM(100, dropout=0.3, return_sequences=True)),
+        Bidirectional(LSTM(100, dropout=0.3, return_sequences=True)),
+        Conv1D(100, 5, activation='relu'),
+        GlobalMaxPool1D(),
+        Dense(16, activation='relu'),
+        Dense(1, activation='sigmoid'),
+    ], name="Sentiment_Model")
+
+    model.load_weights("twitter_sentiment/models/Model Weights")
+    return model
+
+
 def predict(text):
     if isinstance(text, str):
         text = list(text, )
     processed_text = [preprocess_apply(t) for t in text]
     text = pad_sequences(tokenizer.texts_to_sequences(processed_text), maxlen=60)
-    sentiment = model.predict(text)
+    sentiment = sentiment_model.predict(text)
     sentiment = np.where(sentiment > 0.5, 1, -1)
     return processed_text, sentiment
+
+
+with open('twitter_sentiment/models/Tokenizer.pickle', 'rb') as file:
+    tokenizer = pickle.load(file)
+sentiment_model = load_model()
